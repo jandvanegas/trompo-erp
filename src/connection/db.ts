7b90df9d515c7ebcf8292b5228e654549d6e3421
AlexLar163 <61038781+AlexLar163@users.noreply.github.com>
2022-12-13 16:43:03 +0000

    feat(env): added environment variables

--- src/connection/db.ts
+++ src/connection/db.ts
@@ -4,11 +4,11 @@ import { Categories } from '../entities/Categories';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'user',
-  password: 'password',
-  database: 'db',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   entities: [Articles, Categories],
   logging: true,
   synchronize: true,
